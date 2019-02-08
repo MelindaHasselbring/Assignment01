1726ab8a492729bd1bfc810c94b32af2034d039b
MelindaHasselbring <23516873+MelindaHasselbring@users.noreply.github.com>
2019-02-07 20:18:07 -0800

    Assignment01
    Assignment01

---
+++ js/script.js
@@ -0,0 +1,236 @@
+// eslint-disable-next-line no-console
+// STEP 1  Convert the following identifiers to Camel Case notation:
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+
+
+
+
+
+
+// STEP 2 Give me an example of the ff.
+// a.) numeric literal expression
+    var total = 3.14; 
+
+// b.) string literal expression
+    var fName = "Melinda Hasselbring";
+
+// c.) Boolean literal expression
+
+var x = 5;
+var y = 5;
+var z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
+var m = true;
+// d.) null literal expression
+    var x = null;
+
+
+
+// STEP 3 Give me two examples of complex / variable expressions.
+    var GtandTotal = totalAmound + tax;
+    var x = "child" + 16;
+
+
+
+
+// STEP 4.	Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var numYourAge;
+var strReferralSource;
+var strMayWeContactYou;
+
+
+
+// STEP 5.	Take the 9 variables that you created above and demonstrate 3 methods for declaring and assigning values to those variables.
+    strFirstName = "Melinda";
+    strLastName = "Hasselbring";
+    strZipCode = "92110";
+
+
+// STEP 6.	Create a variable. 
+// Add a number and a string and display the coerced result in the browser’s console window. 
+    strAddress = 2017 + "Burgener Blvd.";
+
+// output:
+    "2017 Burgener Blvd."
+
+// STEP 7.	Create two variables. 
+// For the first variable, add a Boolean and a string and display the coerced result. 
+var year = 2019;
+var chineseYear = "pig";
+var yearNow = year == chineseYear + year;
+window.console.log("It's year of the " + chineseYear + ", "+ year);
+
+output:
+"It's year of the pig, 2019"
+
+For the second variable, add a number and a Boolean and display the coerced result. 
+var num = (200==200) + 200;
+num = 400;
+
+
+// STEP 8.	Is the following string literal valid? If not, how would you fix it?
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+window.console.log(someString);
+Ans: by using the escape sequence.
+'Who once said\,"Only two things are infinite\, the universe and human stupidity\, and I\'m not sure about the former."';
+
+
+// STEP 9.	Create a variable that produces a null value in the console window. 
+Create a variable that produces an undefined value in the console window.
+var x = 200;
+var y = null;
+x = null;
+
+window.console.log(x);
+window.console.log(y);
+
+
+// STEP 10.	Use the typeof operator on various literals to return the following types within the console window: 
+// string, 
+typeof "Melinda"
+
+// number, 
+typeof 2019
+
+// Boolean, 
+typeof true
+
+// object, and 
+typeof new Date();
+
+// undefined.
+typeof myAddress
+
+
+
+// STEP 11.	Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows: 
+
+// Hello Zak Ruvalcaba, welcome to the JavaScript class!
+    
+    alert("Hello " + "Melinda Hasselbring" + ", welcome to the JavaScript class!")
+    
+
+// Substitute my name for your name. Although not necessary in practice, I want you to use 2 concatenation operators to construct this string of text. One after the text “Hello” and a second one after your name and before the comma.
+
+
+// STEP 12.	Declare a variable called name and set it equal to your name. 
+// Substitute your name in the previous alert string with the variable instead.
+
+ var fullName = "melinda Hasselbring";
+ alert("Hello " + fullName  + ", welcome to the JavaScript class!")
+
+
+// STEP 13.	Declare a variable called course and set it equal to “JavaScript”. 
+// Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.
+
+ var course = “JavaScript”;
+ alert("Hello " + fullName  + ", welcome to the " + course + " class!")
+
+// STEP 14.	Rework the above string so that a line break is added right before the word “Welcome”. Your alert box should display as follows:
+
+Hello Zak Ruvalcaba.
+Welcome to the JavaScript class!
+
+  alert("Hello " + "Melinda Hasselbring. \nWelcome to the " + course + " class!")
+
+
+
+// STEP 15.	Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable. 
+
+var name = prompt("What is your name?", fullName);
+
+
+// STEP 16.	Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+
+var course = prompt("What course are you taking?", course);
+
+
+// STEP 17.	
+// Assign the value 10 to x on a new line. 
+// Assign the value 20 to y on a new line. 
+// Display the sum of those two numbers in the console window. 
+
+var x = 10;
+var y = 10;
+var total = x + y;
+window.console.log(total);
+
+// output : 20
+
+
+// STEP 18.	
+// Declare a variable called x and assign it a value of 20. 
+// Add and assign 20 to that variable and display the result in the console window.
+// The result should be 40.
+
+var x = 20;
+x = x+20;
+window.console.log(x);
+
+// output : 40
+
+
+
+
+// STEP 19.	
+// Declare a variable called x and assign it a value of 20.
+// Multiply and assign 5 to that variable and display the result in the console window. 
+// The result should be 100.
+
+var x = 20;
+x *=5;
+window.console.log(x);
+
+// output : 100
+
+// STEP 20.	
+// Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.
+// Divide and assign 1 to that variable and display the result in the console window. 
+// The result should be 2. If you got 6.66 try again.
+
+var x = 20;
+x /=3;
+window.console.log(x);
+
+
+
+// STEP 21.	Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+
+var a =2;
+var b =2;
+window.console.log(a == b);
+
+
+// STEP 22.	Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
+
+var a =2;
+var b =2;
+window.console.log(a != b);
+
+
+// STEP 23.	Use the new operator to create a new Object called “widget”. Within the console window, use the typeof operator to display the type of variable widget is.
+var widget = new Object();
+console.log(typeof widget);
+
+
+// STEP 24.	Using the instanceof operator, write an application that returns true within the console window if the widget variable is an instance of an object.
+
+console.log(widget instanceof object);
+
+// STEP 25.	25.	Without modifying the variable declaration and assignment, rewrite the previous application so that it returns false.
+
+console.log(object instanceof widget);
+
